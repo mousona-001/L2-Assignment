d900ff92ceb9e79ba0e647563535404ab0e9546d
Mousona Acharjee <halogen0330@gmail.com>
2023-03-04 22:43:31 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,295 @@
+
+public class Main {
+    public static void main(String[] args){
+
+                // 1. Area of a circle
+//        System.out.print("Enter the radius of the circle: ");
+//        Scanner scn = new Scanner(System.in);
+//        int radius = scn.nextInt();
+//        double area = 0f;
+//        area = 3.14 * radius * radius;
+//        System.out.println("area of the circle is = " + area);
+
+
+                // 2. Area of a triangle
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the value of base of the triangle: ");
+//        int base = scn.nextInt();
+//        System.out.print("Enter the value of height of the triangle: ");
+//        int height = scn.nextInt();
+//        double area = 0f;
+//        area = 0.5 * base * height;
+//        System.out.println("area of the triangle is = " + area);
+
+
+                // 3. Area of rectangle
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the length: ");
+//        int length = scn.nextInt();
+//        System.out.print("Enter the width: ");
+//        int width = scn.nextInt();
+//        double area = 0f;
+//        area = length * width;
+//        System.out.println("area of the rectangle is = " + area);
+
+
+                // 4. Area of Isosceles Triangle
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the value of base of the triangle: ");
+//        int base = scn.nextInt();
+//        System.out.print("Enter the value of height of the triangle: ");
+//        int height = scn.nextInt();
+//        double area = 0f;
+//        area = 0.5 * base * height;
+//        System.out.println("area of the isosceles triangle is = " + area);
+
+
+                // 5. Area of Parallelogram
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the value of base of the parallelogram : ");
+//        int base = scn.nextInt();
+//        System.out.print("Enter the value of height of the parallelogram: ");
+//        int height = scn.nextInt();
+//        double area = 0f;
+//        area = base * height;
+//        System.out.println("area of the parallelogram is = " + area);
+
+
+                // 6. Area of Rhombus
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the d1: ");  // d1 = diagonal 1, d2 = diagonal 2;
+//        int d1 = scn.nextInt();
+//        System.out.print("Enter the d2: ");
+//        int d2 = scn.nextInt();
+//        double area = 0f;
+//        area = 0.5 * d1 * d2;
+//        System.out.println("area of the rhombus is = " + area);
+
+
+                // 7. Area of Equilateral Triangle
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the side of the triangle: ");
+//        int side = scn.nextInt();
+//        double area = 0f;
+//        area = (1.73 * side * side)/4;
+//        System.out.println("area of the equilateral triangle is = " + area);
+
+
+                // 8. Perimeter of Circle
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the radius of the circle: ");
+//        int radius = scn.nextInt();
+//        double perimeter = 0f;
+//        perimeter = 2 * 3.14 * radius;
+//        System.out.println("perimeter of the circle = " + perimeter);
+
+
+                // 9. Perimeter of Equilateral Triangle
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the side of the equilateral triangle : ");
+//        int side = scn.nextInt();
+//        double perimeter = 0f;
+//        perimeter = 3 * side;
+//        System.out.println("perimeter of the circle = " + perimeter);
+
+
+                // 10. Perimeter of Parallelogram
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the value of base of the parallelogram : ");
+//        int base = scn.nextInt();
+//        System.out.print("Enter the value of side of the parallelogram: ");
+//        int side = scn.nextInt();
+//        double perimeter = 0f;
+//        perimeter = 2 * (side + base);
+//        System.out.println("perimeter of the parallelogram is = " + perimeter);
+
+
+                // 11. Perimeter of Rectangle
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the value of length of the rectangle : ");
+//        int length = scn.nextInt();
+//        System.out.print("Enter the value of width of the rectangle: ");
+//        int width = scn.nextInt();
+//        double perimeter = 0f;
+//        perimeter = 2 * (length + width);
+//        System.out.println("perimeter of the rectangle is = " + perimeter);
+
+
+                // 12. Perimeter of Square
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the value of side of the rectangle : ");
+//        int side = scn.nextInt();
+//        double perimeter = 0f;
+//        perimeter = 4 * side;
+//        System.out.println("perimeter of the square is = " + perimeter);
+
+
+                // 13. Perimeter of Rhombus
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the value of side of the rhombus : ");
+//        int side = scn.nextInt();
+//        double perimeter = 0f;
+//        perimeter = 4 * side;
+//        System.out.println("perimeter of the rhombus is = " + perimeter);
+
+
+                // 14. Volume of Cone
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the radius of the cone : ");
+//        int radius = scn.nextInt();
+//        System.out.print("Enter the height of the cone : ");
+//        int height = scn.nextInt();
+//        double volume = 0f;
+//        volume = (3.14 * radius * radius * height)/3;
+//        System.out.println("volume of the cone is = " + volume);
+
+
+                // 15. Volume of Prism
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the base area of the prism : ");
+//        int baseArea = scn.nextInt();
+//        System.out.print("Enter the height of the prism : ");
+//        int height = scn.nextInt();
+//        double volume = 0f;
+//        volume = baseArea * height;
+//        System.out.println("volume of the prism is = " + volume);
+
+
+                // 16. Volume of Cylinder
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the radius of the cylinder : ");
+//        int radius = scn.nextInt();
+//        System.out.print("Enter the height of the cylinder : ");
+//        int height = scn.nextInt();
+//        double volume = 0f;
+//        volume = (3.14 * radius * radius * height);
+//        System.out.println("volume of the cylinder is = " + volume);
+
+
+                // 17. Volume of Sphere
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the radius of the sphere : ");
+//        int radius = scn.nextInt();
+//        double volume = 0f;
+//        volume = (3.14 * radius * radius * radius) * 1.33;
+//        System.out.println("volume of the sphere is = " + volume);
+
+
+                // 18. Volume of Pyramid
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the base length of the pyramid : ");
+//        int baseLength = scn.nextInt();
+//        System.out.print("Enter the base width of the pyramid : ");
+//        int baseWidth = scn.nextInt();
+//        System.out.print("Enter the height of the pyramid : ");
+//        int height = scn.nextInt();
+//        double volume = 0f;
+//        volume = (baseLength * baseWidth * height)/3;
+//        System.out.println("volume of the pyramid is = " + volume);
+
+
+                // 19. Curved Surface Area Of Cylinder
+//        Scanner scn = new Scanner(System.in);
+//        System.out.print("Enter the radius of the cylinder : ");
+//        int radius = scn.nextInt();
+//        System.out.print("Enter the height of the cylinder : ");
+//        int height = scn.nextInt();
+//        double area = 0f;
+//        area = 2 * 3.14 * radius * height;
+//        System.out.println("area of the curved surface of the cylinder is = " + area);
+
+
+                // 20. Total Surface Area Of Cube
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter the side of the cube: ");
+//        int side = scn.nextInt();
+//        double surfaceArea = 0f;
+//        surfaceArea = 6 * side * side;
+//        System.out.println("Surface area of the cube is: " + surfaceArea);
+
+
+                // 21. Fibonacci Series In Java Program
+//        int n1 = 0, n2 = 1, n3, i, count = 10;
+//        System.out.print(n1 + " " + n2);
+//        for(i = 2; i<count; i++) {
+//            n3 = n1 + n2;
+//            System.out.print(" " + n3);
+//            n1 = n2;
+//            n2 = n3;
+//        }
+
+
+                // 22. Subtract the Product and Sum of Digits of an Integer
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter the integer: ");
+//        int n = scn.nextInt(), digit, prod = 1, sum=0;
+//        while(n != 0)
+//        {
+//            digit = n % 10;
+//            sum += digit;
+//            prod *= digit;
+//            n = n/10;
+//        }
+//        int res = prod - sum;
+//        System.out.println("result = " + res);
+
+
+                // 23. Input a number and print all the factors of that number (use loops).
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter the integer: ");
+//        int num = scn.nextInt();
+//        for(int i = 1; i<= num; i++){
+//                if(num % i == 0){
+//                    System.out.println(i);
+//                }
+//        }
+
+
+                // 24. Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+//        Scanner scn = new Scanner(System.in);
+//        int num = 0;
+//        int sum = 0;
+//        while(true){
+//            System.out.print("Enter the integer (Enter '0' to exit): ");
+//            num = scn.nextInt();
+//            if(num == 0){
+//                break;
+//            }
+//            sum += num;
+//        }
+//        System.out.println("Sum of the given integers is: " + sum);
+
+
+//        // 25. Take integer inputs till the user enters 0 and print the largest number from all.
+//        Scanner scn = new Scanner(System.in);
+//        int num = 0;
+//        int sum = 0;
+//        int largest = Integer.MIN_VALUE;
+//        while (true) {
+//            System.out.print("Enter the integer (Enter '0' to exit): ");
+//            num = scn.nextInt();
+//            if (num == 0) {
+//                break;
+//            }
+//            sum += num;
+//            if(num > largest){
+//                largest = num;
+//            }
+//        }
+//        System.out.println("Largest of all is: " + largest);
+
+
+                // 26. Addition Of Two Numbers
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter the first number: ");
+//        int num1 = scn.nextInt();
+//        System.out.println("Enter the second number: ");
+//        int num2 = scn.nextInt();
+//        int sum = num1 + num2;
+//        System.out.println("Sum of the two numbers is: " + sum);
+
+
+            }
+        }
+    }
+}
